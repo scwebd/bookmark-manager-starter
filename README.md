08ac1ae9b7256bc323538cb1d2b1d9c84e7dc1c8
Sarah Cullen <scwebd@gmail.com>
2019-11-11 22:00:45 -0700

    Adding starter code

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript Bookmark Manager
+
+Starter Bookmark Manager code for DU Coding Boot Camp JavaScript Review Session.
+
+## Author
+
+* **Sarah Cullen** - *HTML/CSS/JavaScript*
---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// ==============================
+// GLOBAL VARIABLES
+// ==============================
+var bookmarksStarter = {
+    HTML: [],
+    Git: [],
+    Terminal: [],
+    CSS: [],
+    Bootstrap: [],
+    JavaScript: [],
+    jQuery: [],
+    AJAX: []
+}
+
+// ==============================
+// FUNCTIONS
+// ==============================
+
+
+// ==============================
+// MAIN EXECUTION
+// (tying it all together)
+// ==============================
